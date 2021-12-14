4dd1aa48a0f9d67df7e4bdeb750c9425b80def0b
Johan Masvie <johanmas@stud.ntnu.no>
2021-12-13 22:57:41 -0800

    first commit

---
+++ projects/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from projects.models import Project
+
+def project_index(request):
+    projects = Project.objects.all()
+    context = {
+        'projects': projects,
+    }
+    return render(request, 'projects/index.html', context)
+
+def project_detail(request, pk):
+    project = Project.objects.get(pk=pk)
+    context = {
+        'project': project,
+    }
+    return render(request, 'projects/detail.html', context)
