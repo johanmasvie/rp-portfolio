268f8d8a2ca30dc1ca9e40396061026a6e311d42
Johan Masvie <johanmas@stud.ntnu.no>
2021-12-14 19:52:26 -0800

    cretates html for project_index page

--- projects/views.py
+++ projects/views.py
@@ -6,11 +6,11 @@ def project_index(request):
     context = {
         'projects': projects,
     }
-    return render(request, 'projects/index.html', context)
+    return render(request, 'project_index.html', context)
 
 def project_detail(request, pk):
     project = Project.objects.get(pk=pk)
     context = {
         'project': project,
     }
-    return render(request, 'projects/detail.html', context)
+    return render(request, 'project_detail.html', context)
