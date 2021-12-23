3b0fb5ef58019d6285e228902d649731931cad98
Johan Masvie <johanmas@stud.ntnu.no>
2021-12-23 16:14:47 +0100

    rewriting project image code

--- projects/models.py
+++ projects/models.py
@@ -4,5 +4,5 @@ class Project(models.Model):
     title           = models.CharField(max_length=200)
     description     = models.TextField()
     technology      = models.CharField(max_length=20)
-    image           = models.FilePathField(path="/img")
+    image           = models.ImageField(upload_to="post_images", blank=True)
 
