1fe3acf578b8a2b843490a7a276003f909e1e60e
Johan Masvie <johanmas@stud.ntnu.no>
2021-12-23 21:51:47 +0100

    adding images to image model in projects

--- projects/models.py
+++ projects/models.py
@@ -4,5 +4,4 @@ class Project(models.Model):
     title           = models.CharField(max_length=200)
     description     = models.TextField()
     technology      = models.CharField(max_length=20)
-    image           = models.ImageField(upload_to="post_images", blank=True)
-
+    image           = models.ImageField(upload_to="upload/", blank=True)
